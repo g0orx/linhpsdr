0660a735573313c4f9bedbea2acd553e8816d78f
sq8mvy <sq8mvy@gmail.com>
2018-09-15 13:00:31 +0200

    fixed

--- receiver_dialog.c
+++ receiver_dialog.c
@@ -472,7 +472,7 @@ static gboolean band_select_cb(GtkWidget *widget,gpointer data) {
       frequency_a=10000000LL;
       break;
     default:
-      b=band_get_band(select->choice);
+      b=band_get_band(band);
       mode_a=USB;
       frequency_a=b->frequencyMin;
       lo_a=b->frequencyLO;
